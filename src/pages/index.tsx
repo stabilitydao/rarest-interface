c4c1ae48fc2c7d6d4ee9c077a714dd3024e0913a
Rajkumar9654 <rajkumarcoolx@gmail.com>
2022-01-01 14:50:59 +0530

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen text-white bg-orange-600 flex justify-center items-center">
+      <h1 className="sm:text-7xl text-4xl">RAREST-MARKET</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './src/components/**/*.{ts,tsx,js,jsx}', 
+      './src/pages/**/*.{ts,tsx,js,jsx}'
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
+  
